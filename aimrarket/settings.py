f81c80bba05827a3fef649b766c7edc5cdd47dbe
aouwalitshikkha <aouwalitshikkha@gmail.com>
2026-06-21 14:53:50 +0600

    Add HTML minification middleware

--- aimrarket/settings.py
+++ aimrarket/settings.py
@@ -49,6 +49,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'glossary.middleware.HtmlMinifyMiddleware',
 ]
 
 ROOT_URLCONF = 'aimrarket.urls'
