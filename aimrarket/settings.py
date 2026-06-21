4c31183fde7d19faee41a0ef62de539ac74e1e41
aouwalitshikkha <aouwalitshikkha@gmail.com>
2026-06-21 19:47:14 +0600

    Require token auth for all API endpoints (GET now protected)

--- aimrarket/settings.py
+++ aimrarket/settings.py
@@ -148,7 +148,7 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.SessionAuthentication',
     ],
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAuthenticatedOrReadOnly',
+        'rest_framework.permissions.IsAuthenticated',
     ],
     'DEFAULT_FILTER_BACKENDS': [
         'django_filters.rest_framework.DjangoFilterBackend',
